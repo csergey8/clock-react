3b197e7e8f4c48db5f7e680deb903fc1dd3fecae
csergey8 <c.sergey@protonmail.com>
2019-11-30 19:12:53 +0200

    Updates

---
+++ precache-manifest.e98e0d0e20994f6552f4959971e0011b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f2398303253c963e3b837dd2d819bba",
+    "url": "/clock-react/index.html"
+  },
+  {
+    "revision": "12854d0827d412ad5b98",
+    "url": "/clock-react/static/css/main.e3668759.chunk.css"
+  },
+  {
+    "revision": "649ea70ea9070b102bc7",
+    "url": "/clock-react/static/js/2.da1fc130.chunk.js"
+  },
+  {
+    "revision": "12854d0827d412ad5b98",
+    "url": "/clock-react/static/js/main.a8f728b9.chunk.js"
+  },
+  {
+    "revision": "8fe3b3b13fc122836c9d",
+    "url": "/clock-react/static/js/runtime-main.f78c0142.js"
+  }
+]);
